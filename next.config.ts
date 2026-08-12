a3bfa9659b799ba1ffb4d05ea546b96579dd3cca
kochtaziz <kochtaziz8@gmail.com>
2026-08-12 20:46:38 +0200

    Add GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubPages ? "/oly-drugstore-online-store" : "",
+  assetPrefix: isGithubPages ? "/oly-drugstore-online-store/" : "",
 };
 
 export default nextConfig;
